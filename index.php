2c729fa255b6892f0826d31b29094751e800bd2a
Varshini S <71514075+varshini-varsh@users.noreply.github.com>
2021-05-25 11:36:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,57 @@
+<?php
+//index.php
+$connect = mysqli_connect("localhost", "root", "", "csvtodatabase");
+$message = '';
 
+if(isset($_POST["upload"]))
+{
+ if($_FILES['product_file']['name'])
+ {
+  $filename = explode(".", $_FILES['product_file']['name']);
+  if(end($filename) == "csv")
+  {
+   $handle = fopen($_FILES['product_file']['tmp_name'], "r");
+   while($data = fgetcsv($handle))
+   {
+    $product_id = mysqli_real_escape_string($connect, $data[0]);
+    $product_category = mysqli_real_escape_string($connect, $data[1]);  
+                $product_name = mysqli_real_escape_string($connect, $data[2]);
+    $product_price = mysqli_real_escape_string($connect, $data[3]);
+    $query = "
+     UPDATE daily_product 
+     SET name = '$name', 
+     email = '$email', 
+     password = '$password' 
+     WHERE id = '$id'
+    ";
+    mysqli_query($connect, $query);
+   }
+   fclose($handle);
+   header("location: index.php?updation=1");
+  }
+  else
+  {
+   $message = '<label class="text-danger">Please Select CSV File only</label>';
+  }
+ }
+ else
+ {
+  $message = '<label class="text-danger">Please Select File</label>';
+ }
+}
+
+if(isset($_GET["updation"]))
+{
+ $message = '<label class="text-success">Product Updation Done</label>';
+}
+
+$query = "SELECT * FROM daily_product";
+$result = mysqli_query($connect, $query);
+?>
 <!DOCTYPE html>
 <html>
  <head>
-  <title>CSV to Database</title>
+  <title>Update Mysql Database through Upload CSV File using PHP</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -10,7 +59,7 @@
  <body>
   <br />
   <div class="container">
-   <h2 align="center">Update Mysql Database through Upload CSV File using PHP</a></h2>
+   <h2 align="center">Upload CSV File using PHP</a></h2>
    <br />
    <form method="post" enctype='multipart/form-data'>
     <p><label>Please Select File(Only CSV Formate)</label>
@@ -25,18 +74,18 @@
    <div class="table-responsive">
     <table class="table table-bordered table-striped">
      <tr>
-      <th>Category</th>
-      <th>Product Name</th>
-      <th>Product Price</th>
+      <th>name</th>
+      <th>email</th>
+      <th>password</th>
      </tr>
      <?php
      while($row = mysqli_fetch_array($result))
      {
       echo '
       <tr>
-       <td>'.$row["product_category"].'</td>
-       <td>'.$row["product_name"].'</td>
-       <td>'.$row["product_price"].'</td>
+       <td>'.$row["name"].'</td>
+       <td>'.$row["email"].'</td>
+       <td>'.$row["password"].'</td>
       </tr>
       ';
      }
