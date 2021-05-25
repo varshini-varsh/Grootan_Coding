0deb6322e7ee9e72ad9abc7b6e91037613327ebc
Varshini S <71514075+varshini-varsh@users.noreply.github.com>
2021-05-25 11:49:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
+<!-----------------html------------------------------------------>
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Grootan Coding</title>
+  #ajax
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  #css
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+   #bootstrap
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <br />
+  <div class="container">
+   <h2 align="center">Upload CSV File using PHP</a></h2>
+   <br />
+   <form method="post" enctype='multipart/form-data'>
+    <p><label>Browse...</label>
+    <input type="file" name="product_file" /></p>
+    <br />
+    <input type="submit" name="upload" class="btn btn-info" value="Upload" />
+   </form>  
+  </div>
+ </body>
+</html>
+
+
+<!---------------------------------------php---------------------------------------------------->
 <?php
-//index.php
 $connect = mysqli_connect("localhost", "root", "", "csvtodatabase");
 $message = '';
 
@@ -13,12 +42,12 @@ if(isset($_POST["upload"]))
    $handle = fopen($_FILES['product_file']['tmp_name'], "r");
    while($data = fgetcsv($handle))
    {
-    $product_id = mysqli_real_escape_string($connect, $data[0]);
-    $product_category = mysqli_real_escape_string($connect, $data[1]);  
-                $product_name = mysqli_real_escape_string($connect, $data[2]);
-    $product_price = mysqli_real_escape_string($connect, $data[3]);
+    $id = mysqli_real_escape_string($connect, $data[0]);
+    $name = mysqli_real_escape_string($connect, $data[1]);  
+    $email= mysqli_real_escape_string($connect, $data[2]);
+    $password = mysqli_real_escape_string($connect, $data[3]);
     $query = "
-     UPDATE daily_product 
+     UPDATE data
      SET name = '$name', 
      email = '$email', 
      password = '$password' 
@@ -31,67 +60,19 @@ if(isset($_POST["upload"]))
   }
   else
   {
-   $message = '<label class="text-danger">Please Select CSV File only</label>';
+   $message = '<label class="text-danger"> Select CSV file</label>';
   }
  }
  else
  {
-  $message = '<label class="text-danger">Please Select File</label>';
+  $message = '<label class="text-danger">Select File</label>';
  }
 }
 
 if(isset($_GET["updation"]))
 {
- $message = '<label class="text-success">Product Updation Done</label>';
+ $message = '<label class="text-success">Updated Successfully...</label>';
 }
-
-$query = "SELECT * FROM daily_product";
+$query = "SELECT * FROM data";
 $result = mysqli_query($connect, $query);
 ?>
-<!DOCTYPE html>
-<html>
- <head>
-  <title>Update Mysql Database through Upload CSV File using PHP</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- </head>
- <body>
-  <br />
-  <div class="container">
-   <h2 align="center">Upload CSV File using PHP</a></h2>
-   <br />
-   <form method="post" enctype='multipart/form-data'>
-    <p><label>Please Select File(Only CSV Formate)</label>
-    <input type="file" name="product_file" /></p>
-    <br />
-    <input type="submit" name="upload" class="btn btn-info" value="Upload" />
-   </form>
-   <br />
-   <?php echo $message; ?>
-   <h3 align="center">Deals of the Day</h3>
-   <br />
-   <div class="table-responsive">
-    <table class="table table-bordered table-striped">
-     <tr>
-      <th>name</th>
-      <th>email</th>
-      <th>password</th>
-     </tr>
-     <?php
-     while($row = mysqli_fetch_array($result))
-     {
-      echo '
-      <tr>
-       <td>'.$row["name"].'</td>
-       <td>'.$row["email"].'</td>
-       <td>'.$row["password"].'</td>
-      </tr>
-      ';
-     }
-     ?>
-    </table>
-   </div>
-  </div>
- </body>
-</html>
